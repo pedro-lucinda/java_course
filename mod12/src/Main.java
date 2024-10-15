c3f1e751839431b35cdabc402c88d5333960be00
pedro-lucinda <pedrolucinda96@gmail.com>
2024-10-15 08:27:08 -0300

    mod12

---
+++ mod12/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        ArrayClass arr = new ArrayClass();
+        arr.arr();
+        arr.twoDimensionalArray();
+        arr.loopArr();
+        arr.loopArr2();
+
+        int[] arr2 = {1,2,3,3};
+        System.out.println(arr2.length);
+    }
+}
